1e6bf863475a4f3d4d5d1bab5824d797f173eef9
davide-amabile <davideamabile81@gmail.com>
2020-09-16 15:26:12 +0200

    url

--- js/script.js
+++ js/script.js
@@ -2,7 +2,23 @@ $(document).ready(function(){
   // creare evento click sulla cella della tabella
   $(".square").click(
     function() {
-      alert("ciao");
+      $(".square").addClass("hide");
+      $(this).removeClass("hide");
+
+      $.ajax(
+        {
+        url:https://flynn.boolean.careers/exercises/api/random/int,
+        method: "GET",
+        success: function (data, stato) {
+        $("#risultati").html(data);
+        },
+        error: function (richiesta, stato, errori) {
+        alert("E' avvenuto un errore. " + errore);
+        }
+        }
+        );
+
+
     }
   );
 });

45e506f96d4653354e3bcc7faa83fe14d278c348
davide-amabile <davideamabile81@gmail.com>
2020-09-16 16:16:41 +0200

    stampa numei

--- js/script.js
+++ js/script.js
@@ -2,17 +2,23 @@ $(document).ready(function(){
   // creare evento click sulla cella della tabella
   $(".square").click(
     function() {
+      // nascondo gli tutti gli span al click
       $(".square").addClass("hide");
+      // mostro solo lo span del box del click
       $(this).removeClass("hide");
+
+
+
 
       $.ajax(
         {
-        url:https://flynn.boolean.careers/exercises/api/random/int,
-        method: "GET",
-        success: function (data, stato) {
-        $("#risultati").html(data);
+        "url": "https://flynn.boolean.careers/exercises/api/random/int",
+        "method": "GET",
+        "success": function (data, stato) {
+        // stampo nello span il numero che viene generato
+          $("span").text(data.response);
         },
-        error: function (richiesta, stato, errori) {
+        "error": function (richiesta, stato, errori) {
         alert("E' avvenuto un errore. " + errore);
         }
         }
@@ -22,3 +28,12 @@ $(document).ready(function(){
     }
   );
 });
+
+// creare funzione che se il numero e <= 5 il quadrato è giallo se maggiore verde
+function stampaNumero(num) {
+  if (num <= 5) {
+    $(".square").addClass("yellow");
+  } else {
+    $(".square").addClass("green");
+  }
+}
